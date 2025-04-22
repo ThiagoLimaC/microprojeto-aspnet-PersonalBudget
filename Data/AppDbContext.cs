2fed18131bf6284b0aea2a64ea06000f39bb9fc8
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-04-22 10:55:23 -0300

    Add DbSets and seed data for Etiqueta and Status
    Updated `AppDbContext` with new DbSet properties: `Registros`,
    `Etiquetas`, and `Statuses` to represent database tables.
    Seed data was added in the `OnModelCreating` method for
    `Etiqueta` (5 categories) and `Status` (2 statuses).
    Included `using` directive for the models namespace.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using microprojeto_aspnet_PersonalBudget.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace microprojeto_aspnet_PersonalBudget.Data
 {
@@ -7,6 +8,28 @@ namespace microprojeto_aspnet_PersonalBudget.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
+        }
+
+        public DbSet<Registro> Registros { get; set; }
+        public DbSet<Etiqueta> Etiquetas { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Etiqueta>().HasData(
+                    new Etiqueta { EtiquetaId = "alimentacao", Nome = "Alimentação" },
+                    new Etiqueta { EtiquetaId = "transporte", Nome = "Transporte" },
+                    new Etiqueta { EtiquetaId = "saude", Nome = "Saúde" },
+                    new Etiqueta { EtiquetaId = "moradia", Nome = "Moradia" },
+                    new Etiqueta { EtiquetaId = "lazer", Nome = "Lazer" }
+                );
+
+            modelBuilder.Entity<Status>().HasData(
+                    new Status { StatusId = "pago", Nome = "Pago" },
+                    new Status { StatusId = "nao-pago", Nome = "Não Pago" }
+                );
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
