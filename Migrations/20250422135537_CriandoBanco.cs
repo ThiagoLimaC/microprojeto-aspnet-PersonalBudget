4b7bcb51bb698edcbd4965ce891378ab9521cf5a
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-04-22 10:57:47 -0300

    Add migration to create and seed database schema
    Introduced a new migration `20250422135537_CriandoBanco` to set up
    the database schema for the application. Added tables `Etiquetas`,
    `Statuses`, and `Registros` with relationships and cascading
    deletes. Seeded initial data for categories and statuses. Updated
    `AppDbContextModelSnapshot` and generated the migration designer
    file to reflect the schema and seed data.

---
+++ Migrations/20250422135537_CriandoBanco.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace microprojeto_aspnet_PersonalBudget.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Etiquetas",
+                columns: table => new
+                {
+                    EtiquetaId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Etiquetas", x => x.EtiquetaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    StatusId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.StatusId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Valor = table.Column<float>(type: "real", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataVencimento = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EtiquetaId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    StatusId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Registros_Etiquetas_EtiquetaId",
+                        column: x => x.EtiquetaId,
+                        principalTable: "Etiquetas",
+                        principalColumn: "EtiquetaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Registros_Statuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Statuses",
+                        principalColumn: "StatusId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Etiquetas",
+                columns: new[] { "EtiquetaId", "Nome" },
+                values: new object[,]
+                {
+                    { "alimentacao", "Alimentação" },
+                    { "lazer", "Lazer" },
+                    { "moradia", "Moradia" },
+                    { "saude", "Saúde" },
+                    { "transporte", "Transporte" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Statuses",
+                columns: new[] { "StatusId", "Nome" },
+                values: new object[,]
+                {
+                    { "nao-pago", "Não Pago" },
+                    { "pago", "Pago" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registros_EtiquetaId",
+                table: "Registros",
+                column: "EtiquetaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registros_StatusId",
+                table: "Registros",
+                column: "StatusId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registros");
+
+            migrationBuilder.DropTable(
+                name: "Etiquetas");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+        }
+    }
+}
